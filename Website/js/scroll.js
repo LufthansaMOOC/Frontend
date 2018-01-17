eacde5114856944566f335fe6bd0d473327e0a9a
Tung <michaeltungle@gmail.com>
2018-01-17 00:03:50 +0000

    fix horizontal scroll & card heading problem

--- Website/js/scroll.js
+++ Website/js/scroll.js
@@ -80,6 +80,7 @@ function scrollFunction() {
     }
 }
 
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
